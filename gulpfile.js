c060baa024fc4f469169ed828311e057df74902b
ayhankuru <ayhankuru@yandex.com.tr>
2018-02-22 10:42:59 +0300

    Gulp4 Test

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'),
     apidoc = require('./index.js');
 
-gulp.task('default',function(done){
-    apidoc({
-      src:'./example',
-      dest:'./build',
-      debug:true,
-    },done);
+gulp.task('apidoc', done => {
+    apidoc({ src: './example', dest: './build'}, done);
 });
+
+
+gulp.task('default',
+    gulp.parallel('apidoc')
+);
